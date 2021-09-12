f6e49f369d41f753ee95c7cd18fa918023c9e9d6
e3hope <e3hope@naver.com>
2021-09-12 23:28:27 +0900

    알고리즘 - 거스름돈 문제(완성)

--- 2week/main.java
+++ 2week/main.java
@@ -2,18 +2,24 @@ import java.io.*;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("값을입력하세요");
+        System.out.println("값을 입력하세요.동전개수와 금액");
         Scanner scan = new Scanner(System.in);
-        int request = scan.nextInt();
-        System.out.println(question(request));
+        int coin_count = scan.nextInt();
+        int money = scan.nextInt();
+        int[] coin = new int[coin_count];
+        for( int i=0; i<coin_count; i++){
+            coin[i] = scan.nextInt();
+        }
+        System.out.println(question(coin,coin_count,money));
     }
-    public static int question(int request)
+    public static int question(int[] coin,int coin_count,int money)
     {
         int count = 0;
-        int[] coinTypes = {500, 100, 50, 10};
-        for (int i = 0; i < 4; i++) {
-            count += request / coinTypes[i];
-            request %= coinTypes[i];
+        for (int i = coin_count -1; i >= 0; i--) {
+            if(money>=coin[i]){
+                count += money/coin[i];
+                money = money%coin[i];
+            }
         }
         return count;
     }
--- 2week/main.py
+++ 2week/main.py
@@ -1,11 +1,16 @@
-def question(request):
-    coin = [500,100,50,10]
+def question(coin,money):
     count = 0
     for c in coin:
-        count += request // c
-        request %= c
+        count += money // c
+        money %= c
     return count
 
-print("값을 입력하세요.")
-print(question(int(input())))
+print("값을 입력하세요.동전개수와 금액")
+coin_count, money = map(int, input().split())
+coin = []
+for c in range(coin_count):
+    temp = int(input())
+    coin.append(temp)
+coin.reverse()
+print(question(coin,money))
 # https://github.com/e3hope/Algorithm
